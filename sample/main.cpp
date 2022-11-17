eb0813265752b6593173d5cc593a433510c01e17
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-11-17 21:27:39 +0300

    Initial commit

---
+++ sample/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello, Fourier!");
+    return 0;
+}
---
+++ src/fourier.h
@@ -0,0 +1,5 @@
+#ifndef __FOURIER_H /* fourier.h */
+#define __FOURIER_H
+
+
+#endif __FOURIER_H /* fourier.h */
