fd101aaa54b17fac0b5a820003370952ff0389cd
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-11-24 19:34:51 +0300

    feat(samples): add interactive input

--- sample/main.c
+++ sample/main.c
@@ -5,8 +5,12 @@
 
 int main()
 {
-    big_int a = string_to_big_int("123456789101112131415161718");
-    big_int b = string_to_big_int("181716151413121110987654321");
+    puts("Please enter two numbers to multiply:");
+    char *a_str = NULL, *b_str = NULL;
+    scanf(" %m[0-9] %m[0-9]", &a_str, &b_str);
+
+    big_int a = string_to_big_int(a_str); free(a_str);
+    big_int b = string_to_big_int(b_str); free(b_str);
     big_int c = big_int_mul(&a, &b);
     char *buffer = (char*)calloc(7 * c.chunk_count, sizeof(*buffer));
     big_int_to_string(&a, buffer);
@@ -15,6 +19,7 @@ int main()
     printf("%s = ", buffer);
     big_int_to_string(&c, buffer);
     printf("%s\n", buffer);
+
     big_int_dtor(&a);
     big_int_dtor(&b);
     big_int_dtor(&c);
